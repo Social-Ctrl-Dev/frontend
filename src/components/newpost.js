e653138b970b714b42d6e68f6946b39e78914ca3
monte <montenegrojds@gmail.com>
2023-02-12 23:22:18 -0500

    style for the newpost and the header

--- src/components/newpost.js
+++ src/components/newpost.js
@@ -59,37 +59,3 @@ const NewPost = () => {
 
 export default NewPost;
 
-
-/*
-
-  return (
-    <div className="tweetBox">
-      <form>
-        <div className="tweetBox__input">
-          <Avatar src="https://pbs.twimg.com/profile_images/1077133754345422848/v9k0AolD_400x400.jpg" />
-          <input
-            onChange={(e) => setTweetMessage(e.target.value)}
-            value={tweetMessage}
-            placeholder="What's happening?"
-            type="text"
-          />
-        </div>
-        <input
-          onChange={(e) => setTweetImage(e.target.value)}
-          value={tweetImage}
-          className="tweetBox__imageInput"
-          placeholder="Optional: Enter image URL"
-          type="text"
-        />
-
-        <Button
-          onClick={sendTweet}
-          type="submit"
-          className="tweetBox__tweetButton"
-        >
-          Tweet
-        </Button>
-      </form>
-    </div>
-  );
-*/
